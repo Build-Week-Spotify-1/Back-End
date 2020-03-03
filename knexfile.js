f847086b1ea96ec03f51a54d1f4b09d082a85585
Tristan Depew <tristan.depew@gmail.com>
2020-03-03 17:16:29 -0500

    fixed foreign key truncate error

--- knexfile.js
+++ knexfile.js
@@ -6,6 +6,11 @@ module.exports = {
     useNullAsDefault: true,
     connection: {
       filename: "./data/auth.db3"
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done);
+      }
     }
   },
 
--- seeds/01-users.js
+++ seeds/01-users.js
@@ -1,22 +1,18 @@
 const bcrypt = require("bcryptjs");
 
 exports.seed = function(knex) {
-  return knex("users")
-    .truncate()
-    .then(function() {
-      return knex("users").insert([
-        {
-          username: "testing",
-          password: bcrypt.hashSync("pass", 8),
-          first_name: "test",
-          last_name: "ing"
-        },
-        {
-          username: "joe_123",
-          password: bcrypt.hashSync("word", 8),
-          first_name: "tristan",
-          last_name: "depew"
-        }
-      ]);
-    });
+  return knex("users").insert([
+    {
+      username: "testing",
+      password: bcrypt.hashSync("pass", 8),
+      first_name: "test",
+      last_name: "ing"
+    },
+    {
+      username: "joe_123",
+      password: bcrypt.hashSync("word", 8),
+      first_name: "tristan",
+      last_name: "depew"
+    }
+  ]);
 };
