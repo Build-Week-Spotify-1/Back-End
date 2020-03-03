99ff985e5700803f7577f5963ddbf0c9bd021f48
Tristan Depew <tristan.depew@gmail.com>
2020-03-03 18:14:10 -0500

    added del to users seeds

--- seeds/01-users.js
+++ seeds/01-users.js
@@ -1,18 +1,22 @@
 const bcrypt = require("bcryptjs");
 
 exports.seed = function(knex) {
-  return knex("users").insert([
-    {
-      username: "testing",
-      password: bcrypt.hashSync("pass", 8),
-      first_name: "test",
-      last_name: "ing"
-    },
-    {
-      username: "joe_123",
-      password: bcrypt.hashSync("word", 8),
-      first_name: "tristan",
-      last_name: "depew"
-    }
-  ]);
+  return knex("users")
+    .del()
+    .then(function() {
+      return knex("users").insert([
+        {
+          username: "testing",
+          password: bcrypt.hashSync("pass", 8),
+          first_name: "test",
+          last_name: "ing"
+        },
+        {
+          username: "joe_123",
+          password: bcrypt.hashSync("word", 8),
+          first_name: "tristan",
+          last_name: "depew"
+        }
+      ]);
+    });
 };
